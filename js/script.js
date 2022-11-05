4b4cd5c9a79911d4b2f667300037f40364852524
enrique-specht <enrique.michael.specht@hs-furtwangen.de>
2022-11-05 13:16:50 +0100

    create, edit and delete tasks

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function createProjectPopup() {
+function createPopup() {
     if (document.getElementById("popup-create") != null) {
         return;
     }
-    let popup_create = document.getElementById("create-project-template").content.cloneNode(true).children[0];
+    let popup_create = document.getElementById("popup-create-template").content.cloneNode(true).children[0];
     document.getElementById("main").appendChild(popup_create);
 }
 
@@ -37,7 +37,7 @@ function editProjectPopup(editproject) {
     if (document.getElementById("popup-edit") != null) {
         return;
     }
-    let popup_edit = document.getElementById("edit-project-template").content.cloneNode(true).children[0];
+    let popup_edit = document.getElementById("popup-edit-template").content.cloneNode(true).children[0];
     document.getElementById("main").appendChild(popup_edit);
     
     editproject.setAttribute("edit","");
@@ -71,5 +71,75 @@ function deleteProject() {
     let popup_edit = document.getElementById("popup-edit");
     let project = document.querySelector("[edit]").closest("div.div-project");
     project.remove();
+    popup_edit.remove();
+}
+
+function editTaskPopup(edittask) {
+    if (document.getElementById("popup-edit") != null) {
+        return;
+    }
+    let popup_edit = document.getElementById("popup-edit-template").content.cloneNode(true).children[0];
+    document.getElementById("main").appendChild(popup_edit);
+    
+    edittask.setAttribute("edit","");
+
+    let task_title = edittask.parentNode.children[0].children[0].textContent;
+    let task_duedate = edittask.parentNode.children[0].children[1].textContent;
+    document.getElementById("task-title").value = task_title;
+    document.getElementById("task-duedate").value = task_duedate;
+}
+
+function createOrEditTask() {
+    let backlog = document.getElementById("backlog").children[1];
+    let inProgress = document.getElementById("inProgress").children[1];
+    let finished = document.getElementById("finished").children[1];
+
+    if (document.querySelector("[edit]") == null) {
+        var task = document.getElementById("task-template").content.cloneNode(true).children[0];
+    } else {
+        var task = document.querySelector("[edit]").closest("div.task-item");
+    }
+    let task_title = document.getElementById("task-title").value;
+    let task_duedate = document.getElementById("task-duedate").value;
+    let task_duedate_check = "true";
+    if (document.querySelector("[edit]") == null) {
+        task_duedate_check = document.getElementById("task-duedate-check").checked;
+    }
+
+    if(task_title == "") {
+        return;
+    }
+    if(task_duedate == "" || !task_duedate_check) {
+        task.children[0].children[1].setAttribute("hidden","");
+        task_duedate = "";
+    } else {
+        task.children[0].children[1].removeAttribute("hidden");
+    }
+
+    task.children[0].children[0].textContent = task_title;
+    task.children[0].children[1].textContent = task_duedate;
+
+    if (document.querySelector("[edit]") == null || document.querySelector("[edit]").closest("div.div-task-wrap").id == "backlog") {
+        backlog.appendChild(task);
+    }
+    else if (document.querySelector("[edit]").closest("div.div-task-wrap").id == "inProgress") {
+        inProgress.appendChild(task);
+    }
+    else if (document.querySelector("[edit]").closest("div.div-task-wrap").id == "finished") {
+        finished.appendChild(task);
+    }
+
+    if (document.querySelector("[edit]") == null) {
+        document.getElementById("popup-create").remove();
+    } else {
+        document.getElementById("popup-edit").remove();
+        task.children[1].removeAttribute("edit");
+    }
+}
+
+function deleteTask() {
+    let popup_edit = document.getElementById("popup-edit");
+    let task = document.querySelector("[edit]").closest("div.task-item");
+    task.remove();
     popup_edit.remove();
 }
