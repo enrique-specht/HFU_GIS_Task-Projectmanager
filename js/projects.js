cb66eaedfff8503bdadafe4c00cc9cf13d847de1
enrique-specht <enrique.michael.specht@hs-furtwangen.de>
2022-11-09 17:42:14 +0100

    split script.js into projects.js & tasks.js + changed onclick to eventlisteners

---
+++ js/projects.js
@@ -0,0 +1,82 @@
+let create = document.getElementById("create-new");
+create.addEventListener("click", createProjectPopup)
+function createProjectPopup() {
+    if (document.getElementById("popup-create") != null) {
+        return;
+    }
+    let popup_create = document.getElementById("popup-create-template").content.cloneNode(true).children[0];
+    document.getElementById("main").appendChild(popup_create);
+
+    document.getElementById("project-save").addEventListener("click", createOrEditProject);
+    enableSubmitWithEnter();
+}
+
+let edit = document.getElementsByClassName("project-edit-button");
+for (i of edit) {
+    i.addEventListener("click", editProjectPopup);
+}
+function editProjectPopup(event) {
+    if (document.getElementById("popup-edit") != null) {
+        return;
+    }
+    let popup_edit = document.getElementById("popup-edit-template").content.cloneNode(true).children[0];
+    document.getElementById("main").appendChild(popup_edit);
+    
+    event.currentTarget.setAttribute("edit","");
+
+    let project_title = event.currentTarget.parentNode.children[1].textContent;
+    let project_description = event.currentTarget.parentNode.parentNode.children[1].textContent;
+    document.getElementById("project-title").value = project_title;
+    document.getElementById("project-description").value = project_description;
+
+    document.getElementById("project-save").addEventListener("click", createOrEditProject);
+    document.getElementById("project-delete").addEventListener("click", deleteProject);
+    enableSubmitWithEnter();
+}
+
+function createOrEditProject() {
+    let projectlist = document.getElementById("projectlist");
+    if (document.querySelector("[edit]") == null) {
+        var project = document.getElementById("project-template").content.cloneNode(true).children[0];
+        project.getElementsByClassName("project-edit-button")[0].addEventListener("click", editProjectPopup);
+    } else {
+        var project = document.querySelector("[edit]").closest("div.div-project");
+    }
+    let project_title = document.getElementById("project-title").value;
+    let project_description = document.getElementById("project-description").value;
+
+    if(project_title == "") {
+        return;
+    }
+
+    project.children[0].children[1].textContent = project_title;
+    project.children[1].textContent = project_description;
+
+    projectlist.appendChild(project);
+
+    if (document.querySelector("[edit]") == null) {
+        document.getElementById("popup-create").remove();
+    } else {
+        document.getElementById("popup-edit").remove();
+        project.children[0].children[0].removeAttribute("edit");
+    }
+}
+
+function deleteProject() {
+    let popup_edit = document.getElementById("popup-edit");
+    let project = document.querySelector("[edit]").closest("div.div-project");
+    project.remove();
+    popup_edit.remove();
+}
+
+function enableSubmitWithEnter() {
+    let inputs = document.getElementsByClassName("form-item");
+    for (i of inputs) {
+        i.addEventListener("keypress", function(event) {
+            if (event.key === "Enter") {
+              event.preventDefault();
+              createOrEditProject();
+            }
+          });
+    }
+}
---
+++ js/tasks.js
@@ -0,0 +1,143 @@
+let create = document.getElementById("create-new");
+create.addEventListener("click", createTaskPopup)
+function createTaskPopup() {
+    if (document.getElementById("popup-create") != null) {
+        return;
+    }
+    let popup_create = document.getElementById("popup-create-template").content.cloneNode(true).children[0];
+    document.getElementById("main").appendChild(popup_create);
+
+    document.getElementById("task-save").addEventListener("click", createOrEditTask);
+    enableSubmitWithEnter();
+}
+
+let edit = document.getElementsByClassName("task-edit-div");
+for (i of edit) {
+    i.addEventListener("click", editTaskPopup);
+}
+function editTaskPopup(event) {
+    if (document.getElementById("popup-edit") != null) {
+        return;
+    }
+    let popup_edit = document.getElementById("popup-edit-template").content.cloneNode(true).children[0];
+    document.getElementById("main").appendChild(popup_edit);
+    
+    event.currentTarget.setAttribute("edit","");
+
+    let task_title = event.currentTarget.parentNode.children[0].children[0].textContent;
+    let task_duedate = event.currentTarget.parentNode.children[0].children[1].textContent;
+    document.getElementById("task-title").value = task_title;
+    document.getElementById("task-duedate").value = task_duedate;
+
+    document.getElementById("task-save").addEventListener("click", createOrEditTask);
+    document.getElementById("task-delete").addEventListener("click", deleteTask);
+    enableSubmitWithEnter();
+}
+
+function createOrEditTask() {
+    let backlog = document.getElementById("backlog").children[1];
+    let inProgress = document.getElementById("inProgress").children[1];
+    let finished = document.getElementById("finished").children[1];
+
+    if (document.querySelector("[edit]") == null) {
+        var task = document.getElementById("task-template").content.cloneNode(true).children[0];
+        task.getElementsByClassName("task-edit-div")[0].addEventListener("click", editTaskPopup);
+    } else {
+        var task = document.querySelector("[edit]").closest("div.task-item");
+    }
+    let task_title = document.getElementById("task-title").value;
+    let task_duedate = document.getElementById("task-duedate").value;
+    let task_duedate_check = "true";
+    if (document.querySelector("[edit]") == null) {
+        task_duedate_check = document.getElementById("task-duedate-check").checked;
+    }
+
+    if(task_title == "") {
+        return;
+    }
+    if(task_duedate == "" || !task_duedate_check) {
+        task.children[0].children[1].setAttribute("hidden","");
+        task_duedate = "";
+    } else {
+        task.children[0].children[1].removeAttribute("hidden");
+    }
+
+    task.children[0].children[0].textContent = task_title;
+    task.children[0].children[1].textContent = task_duedate;
+
+    if (document.querySelector("[edit]") == null || document.querySelector("[edit]").closest("div.div-task-wrap").id == "backlog") {
+        backlog.appendChild(task);
+    }
+    else if (document.querySelector("[edit]").closest("div.div-task-wrap").id == "inProgress") {
+        inProgress.appendChild(task);
+    }
+    else if (document.querySelector("[edit]").closest("div.div-task-wrap").id == "finished") {
+        finished.appendChild(task);
+    }
+
+    if (document.querySelector("[edit]") == null) {
+        document.getElementById("popup-create").remove();
+    } else {
+        document.getElementById("popup-edit").remove();
+        task.children[1].removeAttribute("edit");
+    }
+
+    task.addEventListener("dragstart", dragStart);
+    task.addEventListener("dragend", dragEnd);
+}
+
+function deleteTask() {
+    let popup_edit = document.getElementById("popup-edit");
+    let task = document.querySelector("[edit]").closest("div.task-item");
+    task.remove();
+    popup_edit.remove();
+}
+
+function enableSubmitWithEnter() {
+    let inputs = document.getElementsByClassName("form-item");
+    for (i of inputs) {
+        i.addEventListener("keypress", function(event) {
+            if (event.key === "Enter") {
+              event.preventDefault();
+              createOrEditTask();
+            }
+          });
+    }
+}
+
+//Drag and Drop Tasks
+let draggedTask = null;
+
+const tasks = document.querySelectorAll(".task-item");
+tasks.forEach((task) => {
+    task.addEventListener("dragstart", dragStart);
+    task.addEventListener("dragend", dragEnd);
+});
+function dragStart() {
+    draggedTask = this;
+    //console.log("dragStart");
+}
+function dragEnd() {
+    draggedTask = null;
+    //console.log("dragEnd");
+}
+
+const status_stages = document.querySelectorAll(".div-tasklist");
+status_stages.forEach((status) => {
+    status.addEventListener("dragover", function(e) {
+        e.preventDefault();
+        status.appendChild(draggedTask);
+        //console.log("dragOver");
+    });
+    status.addEventListener("dragenter", function() {
+        //console.log("dragEnter");
+    });
+    status.addEventListener("dragleave", function() {
+        //console.log("dragLeave");
+    });
+    status.addEventListener("drop", function() {
+        this.appendChild(draggedTask);
+        //console.log("dragDrop");
+    });
+});
+
